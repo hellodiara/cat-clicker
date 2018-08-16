c0cbc223e4a829c79ddaeea7e482a07d9cbd4535
Diara-Bamba <diarabamba@gmail.com>
2018-08-16 19:09:38 -0400

    Added cat two

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var cat = document.getElementById('cat-photo');
+var cat = document.getElementById('cat-photo-one');
 let clicks = 1;
 
 // Counter clicks increment when user clicks on cat image
 cat.addEventListener('click', function() {
-	document.getElementById('clicks-number').innerHTML = clicks++;
+	document.getElementById('clicks-cat-one').innerHTML = clicks++;
 	console.log('Cat was clicked')
 }, false);
 
