529056bae79728e6e94dc1dbbe4725433ed2294e
Diara-Bamba <diarabamba@gmail.com>
2018-08-16 19:27:53 -0400

    Click functionality catOne + catTwo

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
-var cat = document.getElementById('cat-photo-one');
-let clicks = 1;
+//Cat One
+var catOne = document.getElementById('cat-photo-one');
+let clicksOne = 1;
 
-// Counter clicks increment when user clicks on cat image
-cat.addEventListener('click', function() {
-	document.getElementById('clicks-cat-one').innerHTML = clicks++;
-	console.log('Cat was clicked')
+// Counter clicks increment when user clicks on cat image one
+catOne.addEventListener('click', function() {
+	document.getElementById('clicks-cat-one').innerHTML = clicksOne++;
+	console.log('Cat one was cliked');
 }, false);
 
+//Cat Two
+var catTwo = document.getElementById('cat-photo-two');
+let clicksTwo =1;
+// Counter clicks increment when user clicks on cat image two
+catTwo.addEventListener('click', function() {
+	document.getElementById('clicks-cat-two').innerHTML = clicksTwo++;
+	console.log('Cat two was cliked');
+}, false);
